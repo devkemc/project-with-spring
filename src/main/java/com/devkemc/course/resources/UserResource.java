0b1aea4ac8b69a49d8f7318c1cfd70706f1995d6
Joaquim Kennedy <joaquimkbs@gmail.com>
2023-06-13 21:26:42 -0300

    feat: get all users and get user by id

--- src/main/java/com/devkemc/course/resources/UserResource.java
+++ src/main/java/com/devkemc/course/resources/UserResource.java
@@ -1,17 +1,30 @@
 package com.devkemc.course.resources;
 
 import com.devkemc.course.entities.User;
+import com.devkemc.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Maria","maria@gmail.com", "999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> userList = service.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
